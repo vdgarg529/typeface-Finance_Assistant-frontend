85c689991623c37d42517d5a5cdcb368f8dead9c
vdgarg529 <vdgarg529@gmail.com>
2025-08-31 16:23:08 +0530

    v2.0.0

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000' || "https://typeface-finance-assistant-backend.onrender.com";
